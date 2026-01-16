bb2c7ed6ddfb487baf03997c30064ed9b293a98e
chadchan <112686545+coconutcococode@users.noreply.github.com>
2026-01-16 22:56:51 +0900

    test02-bcy
    test

--- src/test01/Calculator.java
+++ src/test01/Calculator.java
@@ -3,11 +3,11 @@ package test01;
 public class Calculator {
 
     public static void main(String[] args) {
-        int res1 = add(10, 20);
-        int res2 = sub(10, 20);
+        int res1 = add(100, 200);
+        int res2 = sub(100, 200);
 
-        System.out.println(res1);
-        System.out.println(res2);
+        System.out.println("a + b = " + res1);
+        System.out.println("a - b = " + res2);
     }
 
     private static int add(int a, int b) {
