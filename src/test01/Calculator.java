270b104a1500dacb9a1426efb2714af8344399ff
chadchan <112686545+coconutcococode@users.noreply.github.com>
2026-01-16 23:23:02 +0900

    test03-bcy

--- src/test01/Calculator.java
+++ src/test01/Calculator.java
@@ -5,9 +5,11 @@ public class Calculator {
     public static void main(String[] args) {
         int res1 = add(100, 200);
         int res2 = sub(100, 200);
+        int res3 = mul(100, 200);
 
         System.out.println("a + b = " + res1);
         System.out.println("a - b = " + res2);
+        System.out.println("a * b = " + res3);
     }
 
     private static int add(int a, int b) {
@@ -16,5 +18,9 @@ public class Calculator {
 
     private static int sub(int a, int b) {
         return a - b;
+    }
+
+    private static int mul(int a, int b) {
+        return a * b;
     }
 }
